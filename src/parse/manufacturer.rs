2ba4db687151daf0c86c6d6f89f2a27bcb17086f
Lex Robinson <lex@lex.me.uk>
2023-02-19 11:25:37 +0000

    Clean up device name code from recommendations
    I'm told matching on a tuple should produce much better output code

--- src/parse/manufacturer.rs
+++ src/parse/manufacturer.rs
@@ -48,14 +48,12 @@ pub fn unpack_manufacturer_code(packed: u16) -> Result<String> {
 
 const fn pack_manufacturer_code(code: &'static str) -> u16 {
     let code = code.as_bytes();
-    assert!(code.len() == 3);
-    let a = code[0];
-    let b = code[1];
-    let c = code[2];
+    let [a, b, c] = *code else { panic!("Code must be 3 bytes") };
     assert!(
         (a as char).is_ascii_uppercase()
             && (b as char).is_ascii_uppercase()
-            && (c as char).is_ascii_uppercase()
+            && (c as char).is_ascii_uppercase(),
+        "Code must be 3 uppercase letters"
     );
 
     return (a as u16 - 64) * 32 * 32 + (b as u16 - 64) * 32 + (c as u16 - 64);
@@ -94,8 +92,9 @@ const TCH: u16 = pack_manufacturer_code("TCH");
 const WZG: u16 = pack_manufacturer_code("WZG");
 const ZRM: u16 = pack_manufacturer_code("ZRM");
 
-const MBUS_VARIABLE_DATA_MEDIUM_ELECTRICITY: u8 = 0; // TODO
-const MBUS_VARIABLE_DATA_MEDIUM_UNKNOWN: u8 = 1; // TODO
+const MEDIUM_ELECTRICITY: u8 = 0x02;
+const MEDIUM_WARM_WATER: u8 = 0x06;
+const MEDIUM_UNKNOWN: u8 = 0x0F;
 
 pub fn device_name(
     raw_id: [u8; 4],
@@ -103,187 +102,114 @@ pub fn device_name(
     version: u8,
     medium: u8,
 ) -> Option<&'static str> {
-    match manufacturer {
-        ABB => match version {
-            0x02 => Some("ABB Delta-Meter"),
-            0x20 => Some("ABB B21 113-100"),
-            _ => None,
-        },
-        ACW => match version {
-            0x09 => Some("Itron CF Echo 2"),
-            0x0A => Some("Itron CF 51"),
-            0x0B => Some("Itron CF 55"),
-            0x0E => Some("Itron BM +m"),
-            0x0F => Some("Itron CF 800"),
-            0x14 => Some("Itron CYBLE M-Bus 1.4"),
-            _ => None,
-        },
-        AMT => match version {
-            0xC0..=0xFF => Some("Aquametro CALEC ST"),
-            0x80..=0xBF => Some("Aquametro CALEC MB"),
-            0x40..=0x7F => Some("Aquametro SAPHIR"),
-            0x00..=0x3F => Some("Aquametro AMTRON"),
-        },
-        BEC => match medium {
-            MBUS_VARIABLE_DATA_MEDIUM_ELECTRICITY => match version {
-                0x00 => Some("Berg DCMi"),
-                0x07 => Some("Berg BLMi"),
-                _ => None,
-            },
-            MBUS_VARIABLE_DATA_MEDIUM_UNKNOWN => match version {
-                0x71 => Some("Berg BMB-10S0"),
-                _ => None,
-            },
-            _ => None,
-        },
-        EFE => match version {
-            0x00 => match medium {
-                0x06 => Some("Engelmann WaterStar"),
-                _ => Some("Engelmann / Elster SensoStar 2"),
-            },
-            0x01 => Some("Engelmann SensoStar 2C"),
-            _ => None,
-        },
-        ELS => match version {
-            0x02 => Some("Elster TMP-A"),
-            0x0A => Some("Elster Falcon"),
-            0x2F => Some("Elster F96 Plus"),
-            _ => None,
-        },
-        ELV => match version {
-            0x14 | 0x15 | 0x16 | 0x17 | 0x18 | 0x19 | 0x1A | 0x1B | 0x1C | 0x1D => {
-                Some("Elvaco CMa10")
-            }
-            0x32 | 0x33 | 0x34 | 0x35 | 0x36 | 0x37 | 0x38 | 0x39 | 0x3A | 0x3B => {
-                Some("Elvaco CMa11")
-            }
-            _ => None,
-        },
-        EMH => match version {
-            0x00 => Some("EMH DIZ"),
-            _ => None,
-        },
-        EMU => match medium {
-            MBUS_VARIABLE_DATA_MEDIUM_ELECTRICITY => match version {
-                0x10 => Some("EMU Professional 3/75 M-Bus"),
-                _ => None,
-            },
-            _ => None,
-        },
-        GAV => match medium {
-            MBUS_VARIABLE_DATA_MEDIUM_ELECTRICITY => match version {
-                0x2D | 0x2E | 0x2F | 0x30 => Some("Carlo Gavazzi EM24"),
-                0x39 | 0x3A => Some("Carlo Gavazzi EM21"),
-                0x40 => Some("Carlo Gavazzi EM33"),
-                _ => None,
-            },
-            _ => None,
-        },
-        GMC => match version {
-            0xE6 => Some("GMC-I A230 EMMOD 206"),
-            _ => None,
-        },
-        KAM => match version {
-            0x01 => Some("Kamstrup 382 (6850-005)"),
-            0x08 => Some("Kamstrup Multical 601"),
-            _ => None,
-        },
-        SLB => match version {
-            0x02 => Some("Allmess Megacontrol CF-50"),
-            0x06 => Some("CF Compact / Integral MK MaXX"),
-            _ => None,
-        },
-        HYD => match version {
-            0x28 => Some("ABB F95 Typ US770"),
-            0x2F => Some("Hydrometer Sharky 775"),
-            _ => None,
-        },
-        JAN => match medium {
-            MBUS_VARIABLE_DATA_MEDIUM_ELECTRICITY => match version {
-                0x09 => Some("Janitza UMG 96S"),
-                _ => None,
-            },
-            _ => None,
-        },
-        LUG => match version {
-            0x02 => Some("Landis & Gyr Ultraheat 2WR5"),
-            0x03 => Some("Landis & Gyr Ultraheat 2WR6"),
-            0x04 => Some("Landis & Gyr Ultraheat UH50"),
-            0x07 => Some("Landis & Gyr Ultraheat T230"),
-            _ => None,
-        },
-        LSE => match version {
-            0x99 => Some("Siemens WFH21"),
-            _ => None,
-        },
-        NZR => match version {
-            0x01 => Some("NZR DHZ 5/63"),
-            0x50 => Some("NZR IC-M2"),
-            _ => None,
-        },
-        RAM => match version {
-            0x03 => Some("Rossweiner ETK/ETW Modularis"),
-            _ => None,
-        },
-        REL => match version {
-            0x08 => Some("Relay PadPuls M1"),
-            0x12 => Some("Relay PadPuls M4"),
-            0x20 => Some("Relay Padin 4"),
-            0x30 => Some("Relay AnDi 4"),
-            0x40 => Some("Relay PadPuls M2"),
-            _ => None,
-        },
-        RKE => match version {
-            0x69 => Some("Ista sensonic II mbus"),
-            _ => None,
-        },
-        SBC => match raw_id[3] {
-            0x10 | 0x19 => Some("Saia-Burgess ALE3"),
-            0x11 => Some("Saia-Burgess AWD3"),
-            _ => None,
-        },
-        SEO | GTE => match raw_id[3] {
-            0x30 => Some("Sensoco PT100"),
-            0x41 => Some("Sensoco 2-NTC"),
-            0x45 => Some("Sensoco Laser Light"),
-            0x48 => Some("Sensoco ADIO"),
-            0x51 | 0x61 => Some("Sensoco THU"),
-            0x80 => Some("Sensoco PulseCounter for E-Meter"),
-            _ => None,
-        },
-        SEN => match version {
-            0x08 | 0x19 => Some("Sensus PolluCom E"),
-            0x0B => Some("Sensus PolluTherm"),
-            0x0E => Some("Sensus PolluStat E"),
-            _ => None,
-        },
-        SON => match version {
-            0x0D => Some("Sontex Supercal 531"),
-            _ => None,
-        },
-        SPX => match version {
-            0x31 | 0x34 => Some("Sensus PolluTherm"),
-            _ => None,
-        },
-        SVM => match version {
-            0x08 => Some("Elster F2 / Deltamess F2"),
-            0x09 => Some("Elster F4 / Kamstrup SVM F22"),
-            _ => None,
-        },
-        TCH => match version {
-            0x26 => Some("Techem m-bus S"),
-            0x40 => Some("Techem ultra S3"),
-            _ => None,
-        },
-        WZG => match version {
-            0x03 => Some("Modularis ETW-EAX"),
-            _ => None,
-        },
-        ZRM => match version {
-            0x81 => Some("Minol Minocal C2"),
-            0x82 => Some("Minol Minocal WR3"),
-            _ => None,
-        },
+    // HACK: Some manufacturers put the version in a different field
+    let version = match manufacturer {
+        SBC | SEO | GTE => raw_id[3],
+        _ => version,
+    };
+
+    match (manufacturer, version, medium) {
+        // ABB AB
+        (ABB, 0x02, _) => Some("ABB Delta-Meter"),
+        (ABB, 0x20, _) => Some("ABB B21 113-100"),
+        // Actaris, France. (Water and Heat)
+        (ACW, 0x09, _) => Some("Itron CF Echo 2"),
+        (ACW, 0x0A, _) => Some("Itron CF 51"),
+        (ACW, 0x0B, _) => Some("Itron CF 55"),
+        (ACW, 0x0E, _) => Some("Itron BM +m"),
+        (ACW, 0x0F, _) => Some("Itron CF 800"),
+        (ACW, 0x14, _) => Some("Itron CYBLE M-Bus 1.4"),
+        // INTEGRA METERING AG
+        (AMT, 0x00..=0x3F, _) => Some("Aquametro AMTRON"),
+        (AMT, 0x40..=0x7F, _) => Some("Aquametro SAPHIR"),
+        (AMT, 0x80..=0xBF, _) => Some("Aquametro CALEC MB"),
+        (AMT, 0xC0..=0xFF, _) => Some("Aquametro CALEC ST"),
+        // ??? This manufacturer code is not registered
+        (BEC, 0x00, MEDIUM_ELECTRICITY) => Some("Berg DCMi"),
+        (BEC, 0x07, MEDIUM_ELECTRICITY) => Some("Berg BLMi"),
+        (BEC, 0x71, MEDIUM_UNKNOWN) => Some("Berg BMB-10S0"),
+        // Engelmann Sensor GmbH
+        (EFE, 0x00, MEDIUM_WARM_WATER) => Some("Engelmann WaterStar"),
+        (EFE, 0x00, _) => Some("Engelmann / Elster SensoStar 2"),
+        (EFE, 0x01, _) => Some("Engelmann SensoStar 2C"),
+        // Elster GmbH
+        (ELS, 0x02, _) => Some("Elster TMP-A"),
+        (ELS, 0x0A, _) => Some("Elster Falcon"),
+        (ELS, 0x2F, _) => Some("Elster F96 Plus"),
+        // Elvaco AB
+        (ELV, 0x14..=0x1D, _) => Some("Elvaco CMa10"),
+        (ELV, 0x32..=0x3B, _) => Some("Elvaco CMa11"),
+        // EMH metering GmbH & Co. KG (formerly EMH Elektrizitatszahler GmbH & CO KG)
+        (EMH, 0x00, _) => Some("EMH DIZ"),
+        // EMU Elektronik AG
+        (EMU, 0x10, MEDIUM_ELECTRICITY) => Some("EMU Professional 3/75 M-Bus"),
+        // Carlo Gavazzi Controls S.p.A.
+        (GAV, 0x2D..=0x30, MEDIUM_ELECTRICITY) => Some("Carlo Gavazzi EM24"),
+        (GAV, 0x39 | 0x3A, MEDIUM_ELECTRICITY) => Some("Carlo Gavazzi EM21"),
+        (GAV, 0x40, MEDIUM_ELECTRICITY) => Some("Carlo Gavazzi EM33"),
+        // GMC-I Messtechnik GmbH
+        (GMC, 0xE6, _) => Some("GMC-I A230 EMMOD 206"),
+        // Hydrometer GmbH
+        (HYD, 0x28, _) => Some("ABB F95 Typ US770"),
+        (HYD, 0x2F, _) => Some("Hydrometer Sharky 775"),
+        // Janitza electronics GmbH
+        (JAN, 0x09, MEDIUM_ELECTRICITY) => Some("Janitza UMG 96S"),
+        // Kamstrup Energi A/S
+        (KAM, 0x01, _) => Some("Kamstrup 382 (6850-005)"),
+        (KAM, 0x08, _) => Some("Kamstrup Multical 601"),
+        // Landis & Staefa electronic
+        (LSE, 0x99, _) => Some("Siemens WFH21"),
+        // Landis+Gyr GmbH
+        (LUG, 0x02, _) => Some("Landis & Gyr Ultraheat 2WR5"),
+        (LUG, 0x03, _) => Some("Landis & Gyr Ultraheat 2WR6"),
+        (LUG, 0x04, _) => Some("Landis & Gyr Ultraheat UH50"),
+        (LUG, 0x07, _) => Some("Landis & Gyr Ultraheat T230"),
+        // Nordwestdeutsche Zählerrevision Ing. Aug. Knemeyer GmbH & Co. KG
+        (NZR, 0x01, _) => Some("NZR DHZ 5/63"),
+        (NZR, 0x50, _) => Some("NZR IC-M2"),
+        // Rossweiner Armaturen und Messgeräte GmbH & Co. OHG
+        (RAM, 0x03, _) => Some("Rossweiner ETK/ETW Modularis"),
+        // Relay GmbH
+        (REL, 0x08, _) => Some("Relay PadPuls M1"),
+        (REL, 0x12, _) => Some("Relay PadPuls M4"),
+        (REL, 0x20, _) => Some("Relay Padin 4"),
+        (REL, 0x30, _) => Some("Relay AnDi 4"),
+        (REL, 0x40, _) => Some("Relay PadPuls M2"),
+        // Viterra Energy Services (formerly Raab Karcher ES)
+        (RKE, 0x69, _) => Some("Ista sensonic II mbus"),
+        // Saia-Burgess Controls
+        (SBC, 0x10 | 0x19, _) => Some("Saia-Burgess ALE3"),
+        (SBC, 0x11, _) => Some("Saia-Burgess AWD3"),
+        // Sensus Metering Systems
+        (SEN, 0x08 | 0x19, _) => Some("Sensus PolluCom E"),
+        (SEN, 0x0B, _) => Some("Sensus PolluTherm"),
+        (SEN, 0x0E, _) => Some("Sensus PolluStat E"),
+        // SENSOCO Greatech GmbH
+        // GREATech GmbH
+        (SEO | GTE, 0x30, _) => Some("Sensoco PT100"),
+        (SEO | GTE, 0x41, _) => Some("Sensoco 2-NTC"),
+        (SEO | GTE, 0x45, _) => Some("Sensoco Laser Light"),
+        (SEO | GTE, 0x48, _) => Some("Sensoco ADIO"),
+        (SEO | GTE, 0x51 | 0x61, _) => Some("Sensoco THU"),
+        (SEO | GTE, 0x80, _) => Some("Sensoco PulseCounter for E-Meter"),
+        // Schlumberger Industries Ltd.
+        (SLB, 0x02, _) => Some("Allmess Megacontrol CF-50"),
+        (SLB, 0x06, _) => Some("CF Compact / Integral MK MaXX"),
+        // Sontex SA
+        (SON, 0x0D, _) => Some("Sontex Supercal 531"),
+        // Sensus Metering Systems
+        (SPX, 0x31 | 0x34, _) => Some("Sensus PolluTherm"),
+        // AB Svensk Värmemätning SVM
+        (SVM, 0x08, _) => Some("Elster F2 / Deltamess F2"),
+        (SVM, 0x09, _) => Some("Elster F4 / Kamstrup SVM F22"),
+        // Techem Service AG & Co. KG
+        (TCH, 0x26, _) => Some("Techem m-bus S"),
+        (TCH, 0x40, _) => Some("Techem ultra S3"),
+        // Neumann & Co. Wasserzähler Glaubitz GmbH
+        (WZG, 0x03, _) => Some("Modularis ETW-EAX"),
+        // ZENNER International GmbH & Co. KG
+        (ZRM, 0x81, _) => Some("Minol Minocal C2"),
+        (ZRM, 0x82, _) => Some("Minol Minocal WR3"),
         _ => None,
     }
 }
